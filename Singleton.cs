fd9e2bd1c1f082250bfb4cb23f738d0574beb78e
AlfonsPistoletti <45000322+AlfonsPistoletti@users.noreply.github.com>
2023-02-13 00:45:26 +0100

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
